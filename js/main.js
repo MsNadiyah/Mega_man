172e5cfed8e0390fb49c886b75a62b57e19cb5fa
dromero08 <romerodaniel.k90@gmail.com>
2014-12-01 13:11:55 -0800

    megaman on gameboard and timer's there as well

--- js/main.js
+++ js/main.js
@@ -23,3 +23,22 @@ var start = document.getElementById("start");
 
 });
 
+document.addEventListener("onKeyPress", function() {
+var elapsedTime;
+var startTime;
+var endTime;
+var date = new Date();
+
+startTime = Date.getHours() + ":" + Date.getMinutes() + ":" + Date.getSeconds();
+endTime = {
+
+}
+
+elapsedTime = endTime - startTime;
+
+// Displays the elapsed time in the input box
+
+var timer = getDocumentById("timer"); {
+timer.value = elapsedTime;
+}
+});
